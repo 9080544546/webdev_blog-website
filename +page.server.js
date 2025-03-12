067cd069e9a27e36edb69ed9e26376cc1bde8672
bhashkar kumar jha <126100605+9080544546@users.noreply.github.com>
2025-03-12 19:27:04 +0530

    Add files via upload

--- +page.server.js
+++ +page.server.js
@@ -1,62 +1,55 @@
-import { fail, redirect } from "@sveltejs/kit"
-import { User } from "$lib/server/database.js"
-import bcrypt from "bcrypt"
+import { Comment, User, Post } from "$lib/server/database.js"
 
-export const load = async ({ locals }) => {
-  // redirect user if logged in
-  if (locals.user) {
-    throw redirect(307, "/")
+export function load({ params }) {
+  return {
+    post: Post.select({
+      id: true,
+      title: true,
+      content: true,
+      created_at: true,
+      author: {
+        username: true,
+        full_name: true,
+        first_name: true,
+        last_name: true,
+        id: true,
+        avatar_src: true,
+      },
+      tags: { name: true, id: true },
+      image_src: true,
+      comments: {
+        id: true,
+        content: true,
+        created_at: true,
+        author: {
+          full_name: true,
+          avatar_src: true
+        },
+      },
+      filter_single: { slug: params.slug },
+    }),
   }
 }
 
 export const actions = {
   // @ts-ignore
-  default: async ({ request }) => {
+  comment: async ({ request, cookies, params }) => {
     const data = await request.formData()
-    let errors = {}
 
-    let username = data.get("username")
-    if (username.length < 3) {
-      errors["username"] = "Username must be at least 3 characters"
+    let comment = {
+      content: data.get("comment"),
+      author: User.select_query({
+        filter_single: { user_auth_token: cookies.get("session") },
+      }),
+      post: Post.select_query({
+        filter_single: { slug: params.slug },
+      }),
     }
-
-    let email = data.get("email")
-    if (!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
-      errors["email"] = "Invalid email"
-    }
-
-    let password = data.get("password")
-    if (password.length < 5) {
-      errors["password"] = "Password must be at least 5 characters"
-    }
-
-    let confirm_password = data.get("confirm_password")
-    if (password !== confirm_password) {
-      errors["confirm_password"] = "Passwords do not match"
-    }
-
-    if (Object.keys(errors).length > 0) {
-      return fail(422, {
-        username: username,
-        email: email,
-        errors: errors,
-      })
-    }
-
     try {
-      await User.insert({
-        username: username,
-        email: email,
-        password_hash: await bcrypt.hash(password, 10),
-      })
+      await Comment.insert(comment)
+      return { success: "Comment added" }
     } catch (error) {
-      return fail(422, {
-        username: username,
-        email: email,
-        error: error.message,
-      })
+      return { error: error.message }
     }
-
-    throw redirect(307, "/")
   },
 }
